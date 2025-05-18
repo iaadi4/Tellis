0ca2b8326dc3e70aeac8591e01d9a17e38a0bff5
Aditya Singh <adityasingh40675@gmail.com>
2025-05-18 20:43:10 +0530

    feat(server): add verify token and logout method

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,6 +3,9 @@ import authServices from "../services/auth.service";
 import StatusCodes from "../utils/statusCodes.util";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model";
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
@@ -15,6 +18,7 @@ async function login(req: Request, res: Response): Promise<void> {
                 message: "Email or password missing",
                 data: {}
             })
+            return;
         }
         const user = await authServices.login({ email, password }) as User;
         const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "7d" });
@@ -85,7 +89,71 @@ async function register(req: Request, res: Response): Promise<void> {
     }
 }
 
+export async function me(req: Request, res: Response): Promise<void> {
+    try {
+        const token = req.cookies.access_token;
+        if (!token) {
+            res.status(StatusCodes.UNAUTHORIZED).json({
+                success: false,
+                message: "Unauthorized",
+                data: {}
+            });
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+        const user = await prisma.user.findFirst({
+            where: {
+                id: decoded.userId
+            }
+        })
+        if (!user) {
+            res.status(StatusCodes.NOT_FOUND).json({
+                success: false,
+                message: "User not found",
+                data: {}
+            });
+            return;
+        }
+        res.status(StatusCodes.SUCCESS).json({
+            success: true,
+            message: "",
+            data: user
+        });
+    } catch (err) {
+        res.status(StatusCodes.UNAUTHORIZED).json({
+            success: false,
+            message: "Invalid token",
+            data: {}
+        });
+    }
+}
+
+async function logout(req: Request, res: Response): Promise<void> {
+    try {
+        res.clearCookie("access_token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        });
+    
+        res.status(200).json({
+            success: true,
+            message: "User logged out successfully",
+            data: {},
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Logout failed",
+            data: {},
+        });
+    }
+  }
+  
+
 export default {
     login,
-    register
+    register,
+    me,
+    logout
 }
